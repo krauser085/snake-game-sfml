cbd6be79fb56896583941632894243601b092e09
krauser085 <krauser085@gmail.com>
2016-11-14 20:49:47 +0900

    OOP refactoring

--- SnakeSFML/snakegame.cpp
+++ SnakeSFML/snakegame.cpp
@@ -5,47 +5,44 @@ enum direction { stop, left, right, up, down };
 direction d;
 
 sf::Event event;
-
 // window size const
-const int WINDOW_WIDTH = 640;
-const int WINDOW_HEIGHT = 480;
+const sf::Vector2f WINDOW_SIZE(1024, 768);
 
 // for position of the charactor
-float hPostion; // horizontal position
-float vPostion; // vertical position
-				// for current speed of the charactor
-float hSpeed;
-float vSpeed;
+sf::Vector2f position;
+
+sf::Vector2f speed;
+
 // speed limit of the charactor
 const float F_LIMIT = 0.07;
 const float B_LIMIT = -0.07;
 
-sf::Texture image;
+sf::Texture imgHead;
 sf::Sprite imagePlayer;
-sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Green Snake!");
+sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE.x, WINDOW_SIZE.y), "Green Snake!");
 
 // set initial setting
 int initialSetting() {
 	// load image file from project folder into the texture
-	if (!image.loadFromFile("snake.png", sf::IntRect(256, 0, 64, 64))) {
+	if (!imgHead.loadFromFile("snake.png", sf::IntRect(256, 0, 64, 64))) {
 		// if file loading failed show msg
 		std::cout << "there's no file!" << std::endl;
 	}
 
-	imagePlayer.setTexture(image);
+	imagePlayer.setTexture(imgHead);
 	// initial direction
 	d = stop;
 
 	// initial position
-	hPostion = 320;
-	vPostion = 240;
+	position.x = 320;
+	position.y = 240;
 
 	// initial speed;
-	hSpeed = 0;
-	vSpeed = 0;
+	speed.x = 0;
+	speed.y = 0;
 
 	// set initial position of the charactor
-	imagePlayer.setPosition(hPostion, vPostion);
+	imagePlayer.setPosition(position.x, position.y);
 
 	return 0;
 }
@@ -81,45 +78,48 @@ int getInput() {
 	}
 	return 0;
 }
-
-// draw items
-int draw() {
+int logic() {
 	// move circle
 	switch (d) {
 	case left:
-		if (hSpeed > 0) hSpeed = 0;
-		if (hSpeed > B_LIMIT) hSpeed -= 0.0001;
+		if (speed.x > 0) speed.x = 0;
+		if (speed.x > B_LIMIT) speed.x -= 0.0001;
 
-		if (vSpeed > 0) vSpeed -= 0.00005;
-		else if (vSpeed < 0) vSpeed += 0.00005;
+		if (speed.y > 0) speed.y -= 0.00005;
+		else if (speed.y < 0) speed.y += 0.00005;
 		break;
 	case right:
-		if (hSpeed < 0) hSpeed = 0;
-		if (hSpeed < F_LIMIT) hSpeed += 0.0001;
+		if (speed.x < 0) speed.x = 0;
+		if (speed.x < F_LIMIT) speed.x += 0.0001;
 
-		if (vSpeed > 0) vSpeed -= 0.00005;
-		else if (vSpeed < 0) vSpeed += 0.00005;
+		if (speed.y > 0) speed.y -= 0.00005;
+		else if (speed.y < 0) speed.y += 0.00005;
 		break;
 	case up:
-		if (vSpeed > 0) vSpeed = 0;
-		if (vSpeed > B_LIMIT) vSpeed -= 0.0001;
+		if (speed.y > 0) speed.y = 0;
+		if (speed.y > B_LIMIT) speed.y -= 0.0001;
 
-		if (hSpeed > 0) hSpeed -= 0.00005;
-		else if (hSpeed < 0) hSpeed += 0.00005;
+		if (speed.x > 0) speed.x -= 0.00005;
+		else if (speed.x < 0) speed.x += 0.00005;
 		break;
 	case down:
-		if (vSpeed < 0) vSpeed = 0;
-		if (vSpeed < F_LIMIT) vSpeed += 0.0001;
+		if (speed.y < 0) speed.y = 0;
+		if (speed.y < F_LIMIT) speed.y += 0.0001;
 
-		if (hSpeed > 0) hSpeed -= 0.00005;
-		else if (hSpeed < 0) hSpeed += 0.00005;
+		if (speed.x > 0) speed.x -= 0.00005;
+		else if (speed.x < 0) speed.x += 0.00005;
 		break;
 	}
 
-	hPostion += hSpeed;
-	vPostion += vSpeed;
+	position.x += speed.x;
+	position.y += speed.y;
+
+	return 0;
+}
+// draw items
+int draw() {
 	// set position on each frame
-	imagePlayer.setPosition(hPostion, vPostion);
+	imagePlayer.setPosition(position.x, position.y);
 	// clear on every frame
 	window.clear(sf::Color::White);
 	// draw the circle on every frame
@@ -137,6 +137,7 @@ int main() {
 	while (window.isOpen()) {
 		// get input direction from keyboard
 		getInput();
+		logic();
 		// move the charactor with the result of input
 		draw();
 	}
